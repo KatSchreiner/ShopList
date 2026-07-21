a35d8abdad69c7b8db240e6ca390ab171b4e178f
Katerina Schreiner <kat.schreiner@yandex.ru>
2026-07-22 00:00:27 +0300

    feat(ui): добавлен градиентный фон для экрана ShopListViewController - Реализован вертикальный градиент через CAGradientLayer - Логика отрисовки градиента вынесена в GradientBackgroundView - Добавлены Auto Layout ограничения на весь экран

---
+++ ShopList/Screens/ShoppingList/GradientBackgroundView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientBackgroundView.swift
+//  ShopList
+//
+//  Created by Екатерина Шрайнер on 21.07.2026.
+//
+
+import UIKit
+
+class GradientBackgroundView: UIView {
+    private let gradientLayer = CAGradientLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGradientLayer()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = bounds
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGradientLayer()
+    }
+    
+    private func setupGradientLayer() {
+        gradientLayer.colors = [UIColor.slBlue.cgColor, UIColor.white.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+}
--- ShopList/Screens/ShoppingList/ShopListViewController.swift
+++ ShopList/Screens/ShoppingList/ShopListViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ShopListViewController: UIViewController {
-
+    // MARK: - Private Properties
+    private let gradientBackground = GradientBackgroundView()
+    
+    // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
+    }
+    
+    // MARK: - Private Methods
+    private func setupView() {
+        view.addSubview(gradientBackground)
+        gradientBackground.translatesAutoresizingMaskIntoConstraints = false
+        
+        setupConstraint()
     }
 
-
+    private func setupConstraint() {
+        NSLayoutConstraint.activate([
+            gradientBackground.topAnchor.constraint(equalTo: view.topAnchor),
+            gradientBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gradientBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            gradientBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
 }
 
