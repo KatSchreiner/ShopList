e072d4b7aa0977d023d6f0a9ef25a78400c38922
Katerina Schreiner <kat.schreiner@yandex.ru>
2026-07-22 13:50:36 +0300

    fix: Изменено название файла с ShopListViewController на ShoppingListViewController в SceneDelegate

--- ShopList/Screens/ShoppingList/GradientBackgroundView.swift
+++ ShopList/Screens/ShoppingList/GradientBackgroundView.swift
@@ -27,6 +27,7 @@ class GradientBackgroundView: UIView {
     
     private func setupGradientLayer() {
         gradientLayer.colors = [UIColor.slBlue.cgColor, UIColor.white.cgColor]
+        gradientLayer.locations = [0.2, 0.6]
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.0)
         gradientLayer.endPoint = CGPoint(x: 0.5, y: 1.0)
         layer.insertSublayer(gradientLayer, at: 0)
