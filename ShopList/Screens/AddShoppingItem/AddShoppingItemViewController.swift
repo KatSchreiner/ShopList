7c6f243c5c3f00d296aefae65d600baac5ac8b42
Katerina Schreiner <kat.schreiner@yandex.ru>
2026-07-25 16:05:21 +0300

    feat: добавлен градиентный фон в AddShoppingItemViewController

--- ShopList/Screens/AddShoppingItem/AddShoppingItemViewController.swift
+++ ShopList/Screens/AddShoppingItem/AddShoppingItemViewController.swift
@@ -7,7 +7,37 @@
 import UIKit
 
 final class AddShoppingItemViewController: UIViewController {
+    // MARK: - Private Properties
+    private lazy var gradientBackground: GradientBackgroundView = {
+        let backgroundView = GradientBackgroundView(
+            colors: [UIColor.slBlue.cgColor, UIColor.slViolet.cgColor],
+            locations: [0.0, 1.0],
+            startPoint: CGPoint(x: 0.5, y: 0.0),
+            endPoint: CGPoint(x: 0.5, y: 1.0)
+        )
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        return backgroundView
+    }()
+    
+    // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+    
+    // MARK: - Private Methods
+    private func setupView() {
+        view.addSubview(gradientBackground)
+        
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            gradientBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            gradientBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gradientBackground.topAnchor.constraint(equalTo: view.topAnchor),
+            gradientBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
