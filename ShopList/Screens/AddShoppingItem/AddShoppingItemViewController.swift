0696d797e9ee326fb6babf99f476db659308d0b2
Katerina Schreiner <kat.schreiner@yandex.ru>
2026-07-25 21:49:35 +0300

    feat: добавлено текстовое поле добавления товара

--- ShopList/Screens/AddShoppingItem/AddShoppingItemViewController.swift
+++ ShopList/Screens/AddShoppingItem/AddShoppingItemViewController.swift
@@ -19,6 +19,17 @@ final class AddShoppingItemViewController: UIViewController {
         return backgroundView
     }()
     
+    private lazy var itemNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Что купить?"
+        textField.layer.cornerRadius = 10
+        textField.backgroundColor = .white
+        textField.textAlignment = .center
+        textField.clipsToBounds = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
     // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +39,7 @@ final class AddShoppingItemViewController: UIViewController {
     // MARK: - Private Methods
     private func setupView() {
         view.addSubview(gradientBackground)
-        
+        view.addSubview(itemNameTextField)
         addConstraints()
     }
     
@@ -37,7 +48,12 @@ final class AddShoppingItemViewController: UIViewController {
             gradientBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             gradientBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             gradientBackground.topAnchor.constraint(equalTo: view.topAnchor),
-            gradientBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            gradientBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            itemNameTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            itemNameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            itemNameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            itemNameTextField.heightAnchor.constraint(equalToConstant: 70)
         ])
     }
 }
